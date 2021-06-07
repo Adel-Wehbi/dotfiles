907ea6630b62ed6c129c990fb3e0847a4de22e3d
Adel Wehbi <adelwehbi@gmail.com>
2021-06-07 19:18:57 -0400

    Automated second monitor config with xrandr
    There is still a problem when doing a systemctl suspend (aka. sleeping)

--- i3/ui-update.sh
+++ i3/ui-update.sh
@@ -2,6 +2,10 @@
 
 wal -i ~/Pictures/wallpapers
 
-~/.config/polybar/polybar.sh &
 ~/.config/dunst/dunst.sh
-~/.config/i3/firefox/firefoxChrome.sh
+#~/.config/i3/firefox/firefoxChrome.sh
+~/.config/polybar/polybar.sh &
+# restart polybar in case of dual monitors
+# to switch it to the primary monitor
+sleep 20
+~/.config/polybar/polybar.sh &
