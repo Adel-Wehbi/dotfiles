2b4037654f66b0fb1074ba774cce73d9b3647be9
Adel Wehbi <adelwehbi@gmail.com>
2021-06-12 10:12:27 -0400

    polybar update fixed main monitor issue

--- i3/ui-update.sh
+++ i3/ui-update.sh
@@ -5,7 +5,4 @@ wal -i ~/Pictures/wallpapers
 ~/.config/dunst/dunst.sh
 #~/.config/i3/firefox/firefoxChrome.sh
 ~/.config/polybar/polybar.sh &
-# restart polybar in case of dual monitors
-# to switch it to the primary monitor
-sleep 20
-~/.config/polybar/polybar.sh &
+
